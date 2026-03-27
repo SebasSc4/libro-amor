2b2932a3065fe259f76d6789bb52c3af6bf8bee4
SebasSc4 <162903311+SebasSc4@users.noreply.github.com>
2026-03-27 15:02:54 -0600

    cambio 8

--- script.js
+++ script.js
@@ -1,8 +1,18 @@
 const pages = document.querySelectorAll(".page");
+const audio = document.getElementById("musica");
 
 let currentPage = 0;
+let musicStarted = false;
 
 document.addEventListener("click", () => {
+
+  // 🔊 iniciar música SOLO una vez
+  if (!musicStarted) {
+    audio.play().catch(() => {});
+    musicStarted = true;
+  }
+
+  // 📖 pasar página
   if (currentPage < pages.length) {
     pages[currentPage].classList.add("flipped");
     currentPage++;
