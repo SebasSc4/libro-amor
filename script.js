1af4d5ace393f0720ae373df78c47c66864f487d
SebasSc4 <162903311+SebasSc4@users.noreply.github.com>
2026-03-27 14:45:51 -0600

    cambio 5

--- script.js
+++ script.js
@@ -1,18 +1,10 @@
 const pages = document.querySelectorAll(".page");
+
 let currentPage = 0;
-let musicaIniciada = false;
 
-function nextPage() {
-
-  // 🔥 iniciar música SOLO una vez
-  if (!musicaIniciada) {
-    const audio = document.getElementById("musica");
-    audio.play().catch(e => console.log("Audio bloqueado:", e));
-    musicaIniciada = true;
-  }
-
+document.addEventListener("click", () => {
   if (currentPage < pages.length) {
     pages[currentPage].classList.add("flipped");
     currentPage++;
   }
-}
+});
