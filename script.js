38e5e75787b3f552eccaec9de0e6707b0a0ed865
SebasSc4 <162903311+SebasSc4@users.noreply.github.com>
2026-03-27 14:27:15 -0600

    cambio 1

--- script.js
+++ script.js
@@ -1,22 +1,9 @@
-let pages = document.querySelectorAll(".page");
-let current = 0;
-
-const music = document.getElementById("bgMusic");
-let started = false;
+const pages = document.querySelectorAll(".page");
+let currentPage = 0;
 
 function nextPage() {
-
-  if (!started) {
-    music.play().catch(() => {});
-    started = true;
-  }
-
-  if (current < pages.length - 1) {
-    pages[current].classList.add("flip");
-    pages[current].classList.remove("active");
-
-    current++;
-
-    pages[current].classList.add("active");
+  if (currentPage < pages.length) {
+    pages[currentPage].classList.add("flipped");
+    currentPage++;
   }
 }
